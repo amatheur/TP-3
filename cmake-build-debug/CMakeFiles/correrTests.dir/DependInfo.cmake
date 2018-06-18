1f2e7f1493ded6211a4f0721f263446c9a70439b
Manuel Iglesias <iglesiasmanu@gmail.com>
2018-06-17 21:22:30 -0300

    Primera Carga
    todos los archivos

---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Manuel/Documents/alumnos/src/Driver.cpp" "C:/Users/Manuel/Documents/alumnos/cmake-build-debug/CMakeFiles/correrTests.dir/src/Driver.cpp.obj"
+  "C:/Users/Manuel/Documents/alumnos/tests/test_driver.cpp" "C:/Users/Manuel/Documents/alumnos/cmake-build-debug/CMakeFiles/correrTests.dir/tests/test_driver.cpp.obj"
+  "C:/Users/Manuel/Documents/alumnos/tests/test_ventana.cpp" "C:/Users/Manuel/Documents/alumnos/cmake-build-debug/CMakeFiles/correrTests.dir/tests/test_ventana.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../tests/google-test/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Manuel/Documents/alumnos/cmake-build-debug/tests/google-test/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "C:/Users/Manuel/Documents/alumnos/cmake-build-debug/tests/google-test/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrTests.dir/tests/test_driver.cpp.obj"
+  "CMakeFiles/correrTests.dir/tests/test_ventana.cpp.obj"
+  "CMakeFiles/correrTests.dir/src/Driver.cpp.obj"
+  "correrTests.pdb"
+  "correrTests.exe"
+  "correrTests.exe.manifest"
+  "correrTests.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
