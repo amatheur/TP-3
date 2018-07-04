e7f1f03349324be1cbd872570774be52b13f2f32
Manuel Iglesias <iglesiasmanu@gmail.com>
2018-07-04 03:17:19 -0300

    Programas

--- cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -5,6 +5,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/correrTests.dir/tests/test_programa.cpp.obj"
   "CMakeFiles/correrTests.dir/tests/test_ventana.cpp.obj"
   "CMakeFiles/correrTests.dir/src/Calculadora.cpp.obj"
+  "CMakeFiles/correrTests.dir/src/Driver.cpp.obj"
   "CMakeFiles/correrTests.dir/src/Instruccion.cpp.obj"
   "CMakeFiles/correrTests.dir/src/Programa.cpp.obj"
   "correrTests.pdb"
---
+++ src/Driver.cpp
@@ -0,0 +1,85 @@
+//
+// Created by Manuel on 4/7/2018.
+//
+
+#include "Driver.h"
+
+Driver::Driver() {
+
+}
+
+Driver::~Driver() {
+
+}
+
+void Driver::begin(string rutina) {
+
+}
+
+void Driver::end(string rutina) {
+
+}
+
+void Driver::push(int n) {
+
+}
+
+void Driver::add() {
+
+}
+
+void Driver::sub() {
+
+}
+
+void Driver::mul() {
+
+}
+
+void Driver::read(string variable) {
+
+}
+
+void Driver::write(string variable) {
+
+}
+
+void Driver::jump(string rutina) {
+
+}
+
+void Driver::jumpz(string rutina) {
+
+}
+
+void Driver::comenzarEjecucion(string rutina, int capacidadVentana) {
+
+}
+
+void Driver::asignarVariable(string idVariable, int valor) {
+
+}
+
+bool Driver::ejecucionFinalizada() const {
+    return false;
+}
+
+void Driver::ejecutarInstruccionActual() {
+
+}
+
+int Driver::topePila() const {
+    return 0;
+}
+
+int Driver::valorVariable(string idVariable) const {
+    return 0;
+}
+
+int Driver::valorHistoricoVariable(string idVariable, int t) const {
+    return 0;
+}
+
+int Driver::instanteActual() const {
+    return 0;
+}
